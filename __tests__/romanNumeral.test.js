baca74007dcaa4a8bf8fe1aa5cb1e21174e25bf6
Laura Hope <ssteamlaura@gmail.com>
2023-06-20 16:10:54 -0700

    Add passing tests for romanNum logic

--- __tests__/romanNumeral.test.js
+++ __tests__/romanNumeral.test.js
@@ -1,4 +1,4 @@
-import RomanNumeral from '../src/js/romanNumeral.js';
+import { RomanNumeral } from '../src/js/romanNumeral.js';
 
 describe('RomanNumeral', () => {
 
@@ -9,6 +9,10 @@ describe('RomanNumeral', () => {
     expect(RomanNumeral(50)).toEqual("L");
     expect(RomanNumeral(100)).toEqual("C");
     expect(RomanNumeral(1000)).toEqual("M");
+  });
+
+  test('should correctly convert the numbers 2136 to MMCXXXVI', () => {
+    expect(RomanNumeral(2136)).toEqual("MMCXXXVI");
   });
 
 });
--- src/js/romanNumeral.js
+++ src/js/romanNumeral.js
@@ -1,21 +1,98 @@
 // Recursive function logic for Roman Numerals
 
 
-export default function RomanNumeral(number) {
-  
-  if (number === 1) {
-    return "I";
-  } else if (number === 5) {
-    return "V";
-  } else if (number === 10) {
-    return "X";
-  } else if (number === 50) {
-    return "L";
-  } else if (number === 100) {
-    return "C";
-  } else if (number === 1000) {
-    return "M";
-  } else {
-    return "not a roman numeral";
+export const RomanNumeral = (number, romanNum = "") => {
+
+  if (isNaN(number)) {
+    console.log("please enter a number");
+    return;
+  }
+
+  if (number > 3999) {
+    console.log("please enter a number lower than 3999");
+    return;
+  }
+
+  if (number === 0) {
+    console.log(romanNum);
+    return romanNum;
+  }
+
+  if (number >= 1000 && number < 3999) {
+    number -= 1000;
+    romanNum = romanNum.concat("M");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 900 && number < 1000) {
+    number -= 900;
+    romanNum = romanNum.concat("CM");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 500 && number < 900) {
+    number -= 500;
+    romanNum = romanNum.concat("D");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 400 && number < 500) {
+    number -= 400;
+    romanNum = romanNum.concat("CD");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 100 && number < 400) {
+    number -= 100;
+    romanNum = romanNum.concat("C");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 90 && number < 100) {
+    number -= 90;
+    romanNum = romanNum.concat("XC");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 50 && number < 90) {
+    number -= 50;
+    romanNum = romanNum.concat("L");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 40 && number < 50) {
+    number -= 40;
+    romanNum = romanNum.concat("XL");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 10 && number < 40) {
+    number -= 10;
+    romanNum = romanNum.concat("X");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number === 9) {
+    number -= 9;
+    romanNum = romanNum.concat("IX");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 5 && number < 9) {
+    number -= 5;
+    romanNum = romanNum.concat("V");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number === 4) {
+    number -= 4;
+    romanNum = romanNum.concat("IV");
+    return RomanNumeral(number, romanNum);
+  }
+
+  if (number >= 1 && number < 4) {
+    number -= 1;
+    romanNum = romanNum.concat("I");
+    return RomanNumeral(number, romanNum);
   }
 }
